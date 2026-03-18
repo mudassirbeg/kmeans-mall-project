e162a7a9a98a51286761e79a8b452acca8d6615a
Mudassir Beg <mudassirb2631@gmail.com>
2026-03-18 16:46:51 +0530

    Added UI form and fixed prediction display

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import joblib
 import os
 
 app = Flask(__name__)
 
 model = joblib.load("kmeans_model.pkl")
-@app.route("/test")
-def test():
-    pred = model.predict([[60, 70]])
-    return {"cluster": int(pred[0])}
 
+# Home page
 @app.route("/")
 def home():
-    return "Mall Customer KMeans API Running"
+    return render_template("index.html")
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json["input"]
-    prediction = model.predict([data])
-    return jsonify({"cluster": int(prediction[0])})
+# Form prediction
+@app.route("/predict_form", methods=["POST"])
+def predict_form():
+    income = float(request.form["income"])
+    score = float(request.form["score"])
+
+    prediction = model.predict([[income, score]])
+
+    print(prediction)s
+
+    return render_template("index.html", prediction=int(prediction[0]))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
