0760ea48ef36ba6fe2b432a575534126dcb862fa
Mudassir Beg <mudassirb2631@gmail.com>
2026-03-18 16:48:38 +0530

    Final deployment ready

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict_form():
     return render_template("index.html", prediction=int(prediction[0]))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
